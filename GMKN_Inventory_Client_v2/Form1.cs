d40504976d6de11183860cae4b6ffaab874be9ef
Orbán Gábor <o.gabor007@gmail.com>
2023-04-28 16:23:17 +0200

    Add project files.

---
+++ GMKN_Inventory_Client_v2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Hotcakes.CommerceDTO.v1.Catalog;
+using Hotcakes.CommerceDTO.v1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hotcakes.CommerceDTO.v1.Client;
+
+namespace GMKN_Inventory_Client_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string url = "http://20.234.113.211:8100/";
+            string key = "1-67944b35-32ec-4185-83f1-22018c9a1ed1";
+
+            Api proxy = new Api(url, key);
+
+            // find all categories in the store
+            ApiResponse<List<ProductDTO>> response = proxy.ProductsFindAll();
+            foreach(var r in response.Content)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
